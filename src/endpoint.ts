95eadd60eb3c5480c6ba4fdead2b7617084638b1
Guillaume Ducret <guillaumeducret@gmx.com>
2024-03-25 11:29:51 -0400

    Change endpoint url to production

--- src/endpoint.ts
+++ src/endpoint.ts
@@ -35,6 +35,7 @@ export function actionRequestX<A extends Action>(
     const { type, ...body } = action
     const _pathname = pathname(endpoint.pathname, body, { consume: true })
 
+    /*
     let port: number
     switch (subdomain) {
         case 'account':
@@ -56,6 +57,7 @@ export function actionRequestX<A extends Action>(
             port = 49707
             break
     }
+    */
 
     if (_pathname) {
         let _request: Request
@@ -67,14 +69,14 @@ export function actionRequestX<A extends Action>(
 
         if (endpoint.method === 'GET') {
             _request = new Request(
-                `http://localhost:${port}${_pathname}${search(body)}`,
-                //input = `https://${subdomain}.splitflow.workers.dev${_pathname}`
+                //`http://localhost:${port}${_pathname}${search(body)}`,
+                `https://${subdomain}.splitflow.workers.dev${_pathname}${search(body)}`,
                 init
             )
         } else {
             _request = new Request(
-                `http://localhost:${port}${_pathname}`,
-                //input = `https://${subdomain}.splitflow.workers.dev${_pathname}`
+                //`http://localhost:${port}${_pathname}`,
+                `https://${subdomain}.splitflow.workers.dev${_pathname}`,
                 {
                     ...init,
                     method: 'POST',
